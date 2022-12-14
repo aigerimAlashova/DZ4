3890c329ea7eb516886e56d73f0201240480b92c
aigerimAlashova <aigerimalasheva39@gmail.com>
2022-12-14 22:10:43 +0600

    DZ4

---
+++ main.js
@@ -0,0 +1,16 @@
+let add = document.getElementById('increment');
+let remome = document.getElementById('decrement');
+
+let number = document.getElementById('number');
+let num = 0;
+
+let input = document.querySelector('input')
+    input.value = '+= 1;' 
+    let btn=document.querySelector;
+
+    btn.add
+
+
+remome.querySelector('click') 
+    integer -= 1;
+    buttom.innerHTML = integer;
